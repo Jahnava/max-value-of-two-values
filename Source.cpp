75b7ccdd8940765deb5b29319e1ea1f871b37f82
Jahnava <jahnavalmt@gmail.com>
2020-09-18 10:30:41 -0400

    Added cout statement for the max value between three numbers. set up two if else statements that equate whether a number is greater, or else the max is the last value, value3.

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-//functionality description
+//find the max value from two values entered when prompted
 
 #include<iostream>
 #include<string>
@@ -15,7 +15,7 @@ int main()
 	//variables
 	int value1;     //value entered by the user
 	int value2;  //value entered by the user
-
+	int value3;  //value entered by user
 	int max;        //determined value
 
 	//prompt the user for input
@@ -25,21 +25,28 @@ int main()
 	cout << "Enter the second value: ";
 	cin >> value2;
 
+	cout << "Enter the third value: ";
+	cin >> value3;
+
 	//find the maximum value
-	if (value1 > value2)
+	if (value1 > value2 && value1 > value3 ) //value one has to be greater than value 2 and (amperand symbol) greater than value 3 //100 90 80 three values. is 100 greater than 90 and is 100 greater than 80, so this would be true and max would be value 1. 
 	{
 		max = value1;
 	}
-	else
+	else if (value2 > value1 && value2 > value3) //if value two is greater than value one & value 2 is greater than value 3  // ex. 90 100 80 is 100 greater than 90,yes. is 100 greater than 80, true, so this would work
 	{
 		max = value2;
 	}
+	else
+	{
+		max = value3;
+	}//if the max is not value one or value two than value three is the only value left, so max would equal value3.
 
 	//display the result
 	cout << std::fixed << endl << endl;
 
-	cout << "The maximum value between " << value1 << value2
-		<< " is " << max << endl << endl;
+	cout << "The maximum value between " << value1 <<", " << value2 
+		<< ", and " << value3 << " is " << max << endl << endl;
 
 	system("pause");
 	return 0;
